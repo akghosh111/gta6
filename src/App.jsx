4a304d5f3d10b4a5c3682754f574741fab5700ce
akghosh111 <ghoshkiran2017@gmail.com>
2025-05-09 19:20:24 +0530

    adding gsap animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 function App() {
   
+
+  useGSAP(() => {
+    const tl= gsap.timeline();
+
+    tl.to(".vi-mask-group", {
+      rotate: 10,
+      duration: 2,
+      ease: "Power4.easeInOut",
+      transformOrigin: "50% 50%"
+    }).to(".vi-mask-group", {
+      scale:10,
+      duration: 2,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
+      transformOrigin: "50% 50%",
+      opacity: 0,
+
+    })
+  })
 
   return (
     <>
