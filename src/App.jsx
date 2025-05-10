8357e02ee599fce8cf9acd7f88d98136d190715e
akghosh111 <ghoshkiran2017@gmail.com>
2025-05-10 14:13:00 +0530

    hero section animations & content section(text/img)

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,25 @@ function App() {
       }
 
     })
-  })
+  });
+
+  useGSAP(() =>{
+    const main = document.querySelector(".main");
+
+    main?.addEventListener("mousemove", function(e){
+      // console.log(e.clientX, e.clientY);
+      const xMove = (e.clientX / window.innerWidth - 0.5) * 40;
+      gsap.to(".main .text", {
+        x: `${xMove * 0.4}%`
+      });
+      gsap.to(".sky", {
+        x: xMove,
+      });
+      gsap.to(".bg", {
+        x: xMove * 1.7,
+      });
+    });
+  }, [showContent])
 
   return (
     <>
@@ -80,24 +98,39 @@ function App() {
             </div>
             
             <div className="imagesdiv relative overflow-hidden w-full h-screen">
-              <img src="./sky.png" className="absolute top-0 left-0 w-full h-full object-cover" alt="sky image" />
-              <img src="./bg.png" className="absolute top-0 left-0 w-full h-full object-cover" alt="background" />
-              <div className="text text-white flex flex-col gap-3 absolute top-0 left-1/2 -translate-x-1/2">
+              <img src="./sky.png" className="absolute sky scale-[1.2] top-0 left-0 w-full h-full object-cover" alt="sky image" />
+              <img src="./bg.png" className="absolute bg scale-[1.1] top-0 left-0 w-full h-full object-cover" alt="background" />
+              <div className="text text-white flex flex-col gap-3 absolute top-10 left-1/2 -translate-x-1/2">
                 <h1 className="text-[12rem] leading-none -ml-40">grand</h1>
                 <h1 className="text-[12rem] leading-none ml-20">theft</h1>
                 <h1 className="text-[12rem] leading-none -ml-20">auto</h1>
               </div>
-              <img src="./girlbg.png" className="absolute bottom-[-52%] left-1/2 -translate-x-1/2 scale-[0.8]" alt="girl" />
+              <img src="./girlbg.png" className="absolute character bottom-[-52%] left-1/2 -translate-x-1/2 scale-[0.8]" alt="girl" />
 
             
             </div>
             <div className="btmbar text-white absolute bottom-0 left-0 w-full py-15 px-10 bg-gradient-to-t from-black to-transparent">
               <div className="flex gap-4 items-center">
-                <i class="text-4xl ri-arrow-down-line"></i>
+                <i className="text-4xl ri-arrow-down-line"></i>
                 <h3 className="text-xl font-[Helvetica_Now_Display]">Scroll Down</h3>
               </div>
               <img src="./ps5.png" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[65px]" alt="" />
             </div>
+          </div>
+
+          <div className="w-full h-screen flex items-center justify-center px-10 bg-black overflow-hidden">
+            <div className="cntnr flex text-white w-full h-[50%]">
+              <div className="limg relative w-1/2 h-full">
+                <img className="absolute scale-[1.1] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" src="./imag.png" alt="" />
+              </div>
+              <div className="rg w-[30%]">
+                <h1 className="text-6xl">Still Running</h1>
+                <h1 className="text-6xl">Not Hunting</h1>
+                <p className="mt-10 font-[Helvetica_Now_Display] text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, voluptas cum quia pariatur dolores maiores autem molestias eligendi consequatur impedit. Pariatur perferendis perspiciatis iure id quo nihil aliquam sequi culpa!</p>
+                <p className="mt-3 font-[Helvetica_Now_Display] text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed odit officia aspernatur mollitia, nihil eum! Fugiat eaque animi illum esse repudiandae laudantium atque aperiam voluptate, laboriosam dignissimos amet. Illo, sit.</p>
+              </div>
+            </div>
+            
           </div>
 
         </div>
